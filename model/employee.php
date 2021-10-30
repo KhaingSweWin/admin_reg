cf66381f3afe9b3aeaaeff5429f153d1ccf16e65
Khaing Swe Win <khaingswewin@hostmyanmar.net>
2021-10-30 16:29:11 +0630

    add files

---
+++ model/employee.php
@@ -0,0 +1,17 @@
+<?php
+class Employee{
+
+    //variables
+private $name;
+private $ei;
+private $dept;
+// display information
+function display(){
+    echo "Khaing";
+}
+
+}
+//create an object
+$emp1=new Employee();
+$emp1->display();
+?>
